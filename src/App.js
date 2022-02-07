f063d256b67ffec9dda959165ae42d391f06a122
angappanmuthu <angappanmuthu232@gmail.com>
2022-02-07 11:15:37 +0530

    tailwindconfig added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          {/*Download app*/}
+          <section className="bg-black text-white">
+              <div className="flex justify-center py-2">
+                  <span>ZigZag launches Android app.</span>
+                  <span><a className="m-2 bg-yellow-600 border border-yellow-600 border-2 rounded">Download Now</a></span>
+                  <span>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                    </svg>
+                  </span>
+              </div>
+          </section>
+          {/*Navigation bar*/}
+          <section>
+              <div className="m-2">
+                  Test
+              </div>
+          </section>
+      </div>
+  )
 }
-
-export default App;
