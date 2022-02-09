a83f9309046c7bea62c03043728a364461a491f8
angappanmuthu <angappanmuthu232@gmail.com>
2022-02-09 16:36:50 +0530

    code correted

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
           <section className="bg-black text-white">
               <div className="flex justify-center py-2">
                   <span>ZigZag launches Android app.</span>
-                  <span><a className="m-2 bg-yellow-600 border border-yellow-600 border-2 rounded">Download Now</a></span>
+                  <span><button className="m-2 bg-yellow-600 border border-yellow-600 border-2 rounded">Download Now</button></span>
                   <span>
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
