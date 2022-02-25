8b8e1b5c577fd174ed0aa6c76a1c67966cc3f208
angappanmuthu <angappanmuthu232@gmail.com>
2022-02-25 14:39:37 +0530

    api changed

--- src/Pages/TakeOrder.js
+++ src/Pages/TakeOrder.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react';
 import axios from "axios";
 import API from "../api";
 import QRCode from "react-qr-code";
+import {Link} from 'react-router-dom';
 
 function TakeOrder() {
 
