6f487fb1ed0584224a66a9f852316be9bebd0555
harih4ran <hariharank.offl@gmail.com>
2022-03-03 17:15:30 +0530

    staff register ui changed

--- src/Pages/Dashboard/Staf/Style/Styles.js
+++ src/Pages/Dashboard/Staf/Style/Styles.js
@@ -1,15 +1,18 @@
 const styles = {
   title: 'text-black font-semibold text-2xl px-3 mb-2',
   select:
-    'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5',
+    'border px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring-rose-500 w-full ease-linear transition-all duration-150',
   button:
     'mt-2 flex font-bold rounded-md text-sm text-white bg-pink-600 border-pink-600 border-2 hover:border-pink-600 hover:text-pink-600 hover:bg-white border  py-1.5 px-8 focus:outline-none',
-  label:'block mb-2 text-sm font-medium text-gray-900',
+  label: 'block uppercase text-black text-sm font-bold mb-2 border-0',
   pinkbutton:
-    'flex font-bold rounded-md text-sm text-white bg-gradient-to-r from-pink-500 to-pink-600 border-pink-600 border-2 hover:border-pink-600 hover:text-pink-600 hover:bg-gradient-to-r hover:from-white hover:to-white border  py-1.5 w-full flex justify-center focus:outline-none',
-tablehead:'py-3 px-3 text-xs font-medium tracking-wider text-left text-black uppercase',
-redbutton:
-'flex font-bold rounded-md text-sm text-white bg-gradient-to-r from-red-500 to-red-600 border-red-600 border-2 hover:border-red-600 hover:text-red-600 hover:bg-gradient-to-r hover:from-white hover:to-white border  py-1.5 w-full flex justify-center focus:outline-none',
-  }
+    'flex font-bold rounded-md text-sm text-white bg-rose-500 border-pink-600 border-2 hover:border-pink-600 hover:text-pink-600 hover:bg-gradient-to-r hover:from-white hover:to-white border  py-1.5 w-full flex justify-center focus:outline-none',
+  tablehead:
+    'py-3 px-3 text-xs font-medium tracking-wider text-left text-black uppercase',
+  redbutton:
+    'flex font-bold rounded-md text-sm text-white bg-gradient-to-r from-red-500 to-red-600 border-red-600 border-2 hover:border-red-600 hover:text-red-600 hover:bg-gradient-to-r hover:from-white hover:to-white border  py-1.5 w-full flex justify-center focus:outline-none',
+  input:
+    'border-0 border px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring-rose-500 w-full ease-linear transition-all duration-150',
+}
 
 export default styles
