43b906084d6cba27847290cbb4bd9430d13a6918
harih4ran <hariharank.offl@gmail.com>
2022-03-05 13:45:38 +0530

    dashboard label to login label changed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -79,7 +79,7 @@ export default function Home() {
           to="/login"
           className="mx-auto lg:mx-0 hover:underline bg-red-500 text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
         >
-          Dashboard
+          Login
         </Link>
       </>
     )
