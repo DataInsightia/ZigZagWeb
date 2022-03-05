4ce5e7a27023f61a21687036c0b1b4901426f64e
Angappan M <44269293+angappanmuthu@users.noreply.github.com>
2022-03-05 20:20:51 +0530

    material_location Logic added in orderworkstaffcompletion.js

--- src/Pages/Dashboard/Staf/OrderWorkStaffCompletion.js
+++ src/Pages/Dashboard/Staf/OrderWorkStaffCompletion.js
@@ -91,6 +91,14 @@ function OrderWorkStaffCompletion() {
       e.target.assign_stage.value,
       e.target.order_work_label.value,
     )
+
+    // Stroring MaterialLocation in Backend
+
+    axios.post(API+ "/api/material/",{"material_location" : e.target.material_location.value, "staff_id" : staff_id,"order_id" : e.target.order_id.value})
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => console.log(err))
   }
 
   const current = new Date()
@@ -124,7 +132,7 @@ function OrderWorkStaffCompletion() {
                           </div>
                           <div className="lg:w-1/6">
                             <th scope="col" className={styles.tablehead}>
-                              Reference
+                              Sub-works
                             </th>
                           </div>
                           <div className="lg:w-1/6">
@@ -136,6 +144,15 @@ function OrderWorkStaffCompletion() {
                             <th scope="col" className={styles.tablehead}>
                               Stage
                             </th>
+                          </div>
+
+                          <div className="lg:w-1/6">
+                            <th scope="col" className={styles.tablehead}>
+                              Material Location
+                            </th>
+                          </div>
+                          <div className="lg:w-1/6">
+                            <th scope="col" className={styles.tablehead}></th>
                           </div>
                           <div className="lg:w-1/6">
                             <th scope="col" className={styles.tablehead}></th>
@@ -163,13 +180,20 @@ function OrderWorkStaffCompletion() {
                 </div>
 
                 <div className="px-3 w-full md:w-1/2 lg:w-1/6">
+                <input
+                    type="text"
+                   
+                    value={e.orderworkstaffassign.work.work_name}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2"
+                    disabled 
+                  />
                   <input
                     type="text"
                     id="work_id"
                     name="work_id"
                     value={e.orderworkstaffassign.work.work_id}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2"
-                    disabled
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hidden w-full p-2"
+                     hidden
                   />
                 </div>
                 <div className="px-3 w-full md:w-1/2 lg:w-1/6">
@@ -200,6 +224,17 @@ function OrderWorkStaffCompletion() {
                     disabled
                   />
                 </div>
+
+                <div className="px-3 w-full md:w-1/2 lg:w-1/6">
+                  <input
+                    type="text"
+                    id="material_location"
+                    name="material_location"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2"
+                    required
+                  />
+                </div>
+
                 <div className="px-3 w-full md:w-1/2 lg:w-1/6">
                   <div className="flex justify-between">
                     <button type="submit" className={styles.pinkbutton}>
@@ -227,27 +262,27 @@ function OrderWorkStaffCompletion() {
                     <thead>
                       <tr>
                         <div className="flex flex-wrap">
-                          <div className="lg:w-1/5">
+                          <div className="lg:w-1/6">
                             <th scope="col" className={styles.tablehead}>
                               Order
                             </th>
                           </div>
-                          <div className="lg:w-1/5">
+                          <div className="lg:w-1/6">
                             <th scope="col" className={styles.tablehead}>
                               Work
                             </th>
                           </div>
-                          <div className="lg:w-1/5">
+                          <div className="lg:w-1/6">
                             <th scope="col" className={styles.tablehead}>
-                              Reference
+                              Sub-works
                             </th>
                           </div>
-                          <div className="lg:w-1/5">
+                          <div className="lg:w-1/6">
                             <th scope="col" className={styles.tablehead}>
                               Completion Date
                             </th>
                           </div>
-                          <div className="lg:w-1/5">
+                          <div className="lg:w-1/6">
                             <th scope="col" className={styles.tablehead}>
                               Stage
                             </th>
@@ -263,7 +298,7 @@ function OrderWorkStaffCompletion() {
           {completionreview.map((e) => (
             <form onSubmit={onSubmit}>
               <div className="flex flex-wrap">
-                <div className="px-3 w-full md:w-1/2 lg:w-1/5">
+                <div className="px-3 w-full md:w-1/2 lg:w-1/6">
                   <input
                     type="text"
                     id="order_id"
@@ -274,7 +309,7 @@ function OrderWorkStaffCompletion() {
                   />
                 </div>
 
-                <div className="px-3 w-full md:w-1/2 lg:w-1/5">
+                <div className="px-3 w-full md:w-1/2 lg:w-1/6">
                   <input
                     type="text"
                     id="work_id"
@@ -284,7 +319,7 @@ function OrderWorkStaffCompletion() {
                     disabled
                   />
                 </div>
-                <div className="px-3 w-full md:w-1/2 lg:w-1/5">
+                <div className="px-3 w-full md:w-1/2 lg:w-1/6">
                   <input
                     type="text"
                     id="order_work_label"
@@ -294,7 +329,7 @@ function OrderWorkStaffCompletion() {
                     disabled
                   />
                 </div>
-                <div className="px-3 w-full md:w-1/2 lg:w-1/5">
+                <div className="px-3 w-full md:w-1/2 lg:w-1/6">
                   <input
                     name="date"
                     value={date}
@@ -302,7 +337,7 @@ function OrderWorkStaffCompletion() {
                     disabled
                   />
                 </div>
-                <div className="px-3 w-full md:w-1/2 lg:w-1/5">
+                <div className="px-3 w-full md:w-1/2 lg:w-1/6">
                   <input
                     type="text"
                     id="assign_stage"
