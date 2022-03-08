1fcd08f7ed22714be460a1d5da5860e8226c9a54
Angappan M <44269293+angappanmuthu@users.noreply.github.com>
2022-03-08 18:58:02 +0530

    changes in order_status.js in admin

--- src/Pages/Dashboard/Admin/Order_Status.js
+++ src/Pages/Dashboard/Admin/Order_Status.js
@@ -55,11 +55,11 @@ function OrderStatus() {
                                 <div className="flex flex-wrap justify-between items-center mt-20">
                                     <div className="inline-flex items-center">
                                         <div className="w-10 h-10 rounded-full overflow-hidden flex-shrink-0">
-                                            <img src="https://randomuser.me/api/portraits/men/5.jpg"/>
+                                            {/* <img src="https://randomuser.me/api/portraits/men/5.jpg"/> */}
                                         </div>
                                             <div className="flex-1 pl-2">
-                                                <h2 className="text-white mb-1">Luke Nunez</h2>
-                                                <p className="text-white opacity-50 text-xs">May 18</p>
+                                                <h2 className="text-white mb-1">{localStorage.getItem('role')}</h2>
+                                                {/* <p className="text-white opacity-50 text-xs">May 18</p> */}
                                             </div>
                                         </div>
                                         <span className="text-white opacity-50">
