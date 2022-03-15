132759b1af8f41d08136aafecdb60e001838a0ea
Angappan M <44269293+angappanmuthu@users.noreply.github.com>
2022-03-15 16:40:08 +0530

    conflict corrected

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 
-scp -P 21098 -r build/* dataioit@datainsightia.in:/home/dataioit/ZigZagBackend/
+rm -rf node_modules/
+rm -rf package-lock.json
+npm i
+npm run build
+scp -P 21098 -r build/* dataioit@datainsightia.in:/home/dataioit/chettinadzizag.in/
