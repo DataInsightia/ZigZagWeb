385321fef178a3e266c63f4efc22a7f36f380cc3
Angappan M <44269293+angappanmuthu@users.noreply.github.com>
2022-03-15 15:58:29 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
-
-scp -P 21098 -r build/* dataioit@datainsightia.in:/home/dataioit/ZigZagBackend/
+rm -rf node_modules/
+rm -rf package-lock.json
+npm i
+npm run build
+scp -P 21098 -r build/* dataioit@datainsightia.in:/home/dataioit/chettinadzigzag.in/
