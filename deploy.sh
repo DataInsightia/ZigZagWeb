8c4f8f7dd20983b60513feed3405a189fb66d6f7
Angappan M <44269293+angappanmuthu@users.noreply.github.com>
2022-03-15 16:42:24 +0530

    domain modified

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ rm -rf node_modules/
 rm -rf package-lock.json
 npm i
 npm run build
-scp -P 21098 -r build/* dataioit@datainsightia.in:/home/dataioit/chettinadzizag.in/
+scp -P 21098 -r build/* dataioit@datainsightia.in:/home/dataioit/chettinadzigzag.in/
