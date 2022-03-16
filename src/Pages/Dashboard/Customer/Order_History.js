28320b6d267bd2067699dd61a7a7f4f9793027f7
Kalidass <kalidass@awventxr.com>
2022-03-16 17:20:26 +0530

    ui changes 5.06pm

--- src/Pages/Dashboard/Customer/Order_History.js
+++ src/Pages/Dashboard/Customer/Order_History.js
@@ -62,21 +62,21 @@ const CustomerOrderHistory = () => {
                                 </thead>
                                 <tbody>
                                 {
-                                    orders.map(e => 
-                                    <Link
-                                        to={"/dashboard/invoice/" + custid +"/" + e.order_id}
-                                        className="font-bold text-lg text-gray-400 block py-2.5 px-4 rounded transition duration-200 hover:bg-rose-50 hover:text-pink-500"
-                                        >
-                                            {/* {e.order_id} {e.work_name} x {e.quantity} */}
+                                    orders.map(e =>
                                             <tr>
-                                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <td class="px-auto py-5 border-b border-gray-200 bg-white text-sm">
                                                     <div class="flex">
                                                         <div class="flex-shrink-0 w-10 h-10">
-                                                            <img
-                                                                class="w-full h-full rounded-full"
-                                                                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                                                                alt=""
-                                                            />
+                                                            <Link
+                                                            to={"/dashboard/invoice/" + custid +"/" + e.order_id}
+                                                            className="w-full h-full rounded-full"
+                                                            >
+                                                                <img
+                                                                    className="w-full h-full rounded-full"
+                                                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                                                                    alt=""
+                                                                />
+                                                            </Link>
                                                         </div>
                                                         <div class="ml-3">
                                                             <p class="text-gray-900 whitespace-no-wrap">
@@ -115,25 +115,9 @@ const CustomerOrderHistory = () => {
                                                 <span className="relative">Deliverd</span>
                                                 </span>
                                             </td>
-                                            <td
-                                                class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right"
-                                            >
-                                                <button
-                                                    type="button"
-                                                    class="inline-block text-gray-500 hover:text-gray-700"
-                                                >
-                                                    <svg
-                                                        class="inline-block h-6 w-6 fill-current"
-                                                        viewBox="0 0 24 24"
-                                                    >
-                                                        <path
-                                                            d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z"
-                                                        />
-                                                    </svg>
-                                                </button>
-                                            </td>
                                         </tr>
-                                        </Link>)
+
+                                    )
                                 }
                                 </tbody>
                             </table>
