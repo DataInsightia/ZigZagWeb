72d0b4d43145de247acd0758b8abf1313275b1a8
Angappan M <44269293+angappanmuthu@users.noreply.github.com>
2022-03-16 11:35:42 +0530

    Additional SideBar.js->Wage removed

--- src/Pages/Dashboard/Sidebar.js
+++ src/Pages/Dashboard/Sidebar.js
@@ -113,12 +113,7 @@ export default function Sidebar() {
                   >
                     WAGE
                   </Link>
-                  <Link
-                    to="/dashboard/wage"
-                    className="font-bold text-lg text-gray-400 block py-2.5 px-4 rounded transition duration-200 hover:bg-rose-50 hover:text-pink-500"
-                  >
-                    WAGE
-                  </Link>
+                  
                   <Link
                     to="/dashboard/staff_register"
                     className="font-bold text-lg text-gray-400 block py-2.5 px-4 rounded transition duration-200 hover:bg-rose-50 hover:text-pink-500"
