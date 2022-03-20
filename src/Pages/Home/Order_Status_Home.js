b2c5b8eee1298eb2a6df22925d30f996d5914bf7
Angappan M <44269293+angappanmuthu@users.noreply.github.com>
2022-03-20 13:03:40 +0530

    HomeOrderStatus modified

--- src/Pages/Home/Order_Status_Home.js
+++ src/Pages/Home/Order_Status_Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Order_Status from "../Dashboard/Admin/Order_Status";
+import CustomerOrderStatus from '../Dashboard/Customer/CustomerOrderStatus';
 
 const OrderStatusHome = () => {
     return (
         <div>
             <Navbar/>
             <div className="w-full justify-center mt-24 mb-8">
-                <Order_Status/>
+                <CustomerOrderStatus />
             </div>
             <Footer/>
         </div>
